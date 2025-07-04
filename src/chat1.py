7ab368e6f5b1929a27a6781000f73cf6782f6085
Takanobu Ugajin <blackdesign.graphicdesign@gmail.com>
2025-07-04 12:44:55 +0900

    問いを「東京スカイツリーの高さは？」に変更

--- src/chat1.py
+++ src/chat1.py
@@ -2,11 +2,16 @@
 from llama_index.llms.ollama import Ollama
 
 def main():
+    # Ollamaモデルの初期化: Ollamaクラスをschroneko/gemma-2-2b-jpn-itというモデル名で初期化。
+    # llama_indexのOllamaクラスのインスタンスを作成し、llmという変数に代入。Ollamaと連携するためのオブジェクトが生成
     llm = Ollama(
+        # 初期化するOllamaモデルの名前を指定
         model="schroneko/gemma-2-2b-jpn-it"
     )
 
-    response = llm.complete('富士山の標高は？')
+    # LLMへの問い合わせ
+    response = llm.complete('東京スカイツリーの高さは？')
+    # LLMからの応答を変数responseに格納し、出力
     print(response)
 
 if __name__ == "__main__":
